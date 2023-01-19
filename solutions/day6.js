67998502564eddbeb8df6f064dae40bf10063469
ryan-heslin <rwheslin@gmail.com>
2023-01-18 20:07:31 -0500

    Enough JavaScript

---
+++ solutions/day6.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function solve(state){
+    let i = 1;
+    let seen = {};
+    seen[state.toString()] = 0;
+    let size = state.length;
+
+    while(true){
+        //https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array
+        // index of max
+        let target = state.reduce((imax, x, i, state) => x > state[imax] ? i : imax, 0);
+        let n_blocks = state[target];
+        let per_block = Math.floor(n_blocks / size);
+        let leftover = n_blocks % size;
+        state[target] = 0;
+
+        for(let i=1; i <= size; i++){
+            state[(target + i) % size] += per_block + (leftover > 0);
+            leftover --;
+        }
+        //console.log(state);
+        let record = state.toString();
+        if(record in seen){
+            return [i, i - seen[record] ] ;
+        }
+        seen[record] = i;
+        i ++;
+    }
+
+    return i;
+}
+
+const raw_input = fs.readFileSync('inputs/day6.txt', 'utf-8').toString().replace("\n", "").split("\t");
+const state = raw_input.map(Number);
+const answer = solve(state);
+console.log(answer[0]);
+console.log(answer[1]);
---
+++ solutions/day8.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const registers = {};
+
+const ops = {">=" : function(lhs, rhs) {return lhs >= rhs;},
+">" :  function(lhs, rhs) {return lhs > rhs;},
+"<="  :function(lhs, rhs) {return lhs <= rhs;},
+"<"  :function(lhs, rhs) {return lhs < rhs;},
+"!="  :function(lhs, rhs) {return lhs != rhs;},
+"==" : function(lhs, rhs) {return lhs == rhs
+;}
+}
+
+function as_key(chars){
+    return "['" + chars + "']"
+}
+function parse(line){
+    let parts = line.split(" ");
+    parts[2] = Number(parts[2]);
+    parts[6] = Number(parts[6]);
+    parts[2] = parts[1] == "inc" ? parts[2] : -parts[2]
+   return {"target": parts[0], "addend" : parts[2], "comparison" : parts[4], "operator" : ops[parts[5]], "compare_val" : parts[6]}
+}
+const raw_input = fs.readFileSync('inputs/day8.txt', 'utf-8').toString();
+const instructions = raw_input.split("\n").map(parse);
+instructions.pop();
+let part2 = 0;
+
+for(const line of instructions){
+    if(! (line["target"] in registers)){
+        registers[line["target"]] = 0;
+    }
+    if(! (line["comparison"] in registers)){
+        registers[line["comparison"]] = 0;
+    }
+
+    registers[line["target"]] += line["operator"](registers[line["comparison"]], line["compare_val"]) * line["addend"]
+    part2 = Math.max(registers[line["target"]], part2);
+}
+
+const part1 = Math.max(...Object.values(registers));
+console.log(part1);
+console.log(part2);
