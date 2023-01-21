4946565b45e6e7f2c9a21b16b43fcb156f4921a1
ryan-heslin <rwheslin@gmail.com>
2023-01-21 09:52:39 -0500

    Day 10

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,29 +1,67 @@
-def knot_hash(lengths):
+from functools import reduce
+from operator import xor
+
+
+def knot_hash(lengths, n, rounds=1):
     current = skip_size = 0
-    numbers = list(range(5))
-    n = len(numbers)
+    numbers = list(range(n))
 
-    for length in lengths:
-        if length <= n:
-            end = current + length
-            overlap_end = end % n
-            if end == overlap_end:
-                numbers[current : (end + 1)] = reversed(numbers[current : (end + 1)])
-            else:
-                reversal = numbers[current:] + numbers[: (overlap_end + 1)]
+    for _ in range(rounds):
 
-            numbers[current]
-            current += skip_size + length
-            current %= n
-            skip_size += 1
+        for length in lengths:
+            # Add length MINUS 1 to get stop index
+            # e.g., length 1 selects element itself
+            if length <= n:
+                end = current + length - 1
+                overlap_end = end % n
+                if end < n:
+                    numbers[current : (end + 1)] = reversed(
+                        numbers[current : (end + 1)]
+                    )
+                else:
+                    reversal = numbers[current:] + numbers[: (overlap_end + 1)]
+                    numbers[: overlap_end + 1] = reversal[overlap_end::-1]
+                    numbers[current:] = reversal[
+                        : len(reversal) - (n - current + 1) : -1
+                    ]
+
+                current += skip_size + length
+                current %= n
+                skip_size += 1
     return numbers
+
+
+def complete_hash(lengths, n=256, rounds=1):
+    chunk_size = 16
+    lengths = process_input(lengths)
+    hashed = knot_hash(lengths, n, rounds=64)
+    dense_hash = [
+        reduce_xor(hashed[i : (i + chunk_size)]) for i in range(0, n, chunk_size)
+    ]
+    return finish_hash(dense_hash)
+
+
+def reduce_xor(xs):
+    return reduce(xor, xs)
+
+
+def process_input(inp):
+    return list(ord(x) for x in inp) + [17, 31, 73, 47, 23]
+
+
+def finish_hash(numbers):
+    return "".join(hex(x)[2:].zfill(2) for x in numbers)
 
 
 with open("inputs/day10.txt") as f:
     raw_input = f.read()
 
 lengths = [int(x) for x in raw_input.split(",")]
-lengths = [3, 4, 1, 5]
-hashed = knot_hash(lengths)
+# lengths = [3, 4, 1, 5]
+n = 256
+hashed = knot_hash(lengths, n)
 part1 = hashed[0] * hashed[1]
 print(part1)
+
+part2 = complete_hash(raw_input.rstrip("\n").lstrip("\n"), n=n, rounds=64)
+print(part2)
