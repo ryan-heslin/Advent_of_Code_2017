d1312e45080c7ba772549c0f70a1abe169e66c35
ryan-heslin <rwheslin@gmail.com>
2023-01-22 12:27:59 -0500

    Day 20

---
+++ solutions/day20.R
@@ -0,0 +1,57 @@
+parse_line <- function(line, dimnames, nrow) {
+    gsub("[<>a-z=]", "", line) |>
+        strsplit(",") |>
+        unlist(use.names = FALSE, recursive = FALSE) |>
+        strtoi() |>
+        matrix(nrow = nrow, dimnames = dimnames)
+}
+
+minimize <- function(data, iterations) {
+    prev <- sum(abs(data[, "p"]))
+    m <- nrow(data)
+    n <- ncol(data)
+
+
+    multiplier <- c(1, iterations, choose(iterations + 1, 2))
+    result <- data %*% diag(multiplier)
+    .rowSums(result, m = m, n = n) |>
+        abs() |>
+        sum()
+}
+
+simulate <- function(particles, iterations) {
+    for (i in seq_len(iterations)) {
+        sequence <- seq_len(nrow(particles))
+        for (j in sequence) {
+            particles[, "v"][[j]] <- particles[, "v"][[j]] + particles[, "a"][[j]]
+            particles[, "p"][[j]] <- particles[, "p"][[j]] + particles[, "v"][[j]]
+        }
+        hashes <- vapply(particles[, "p"],
+            \(x) paste(x, collapse = ","),
+            FUN.VALUE = character(1)
+        )
+        counts <- table(hashes)
+        duplicates <- names(counts[counts > 1])
+        particles <- particles[!hashes %in% duplicates, ]
+    }
+    nrow(particles)
+}
+
+
+
+raw_input <- readLines("inputs/day20.txt")
+
+vectors <- strsplit(raw_input, ",\\s")
+dimnames <- list(c("x", "y", "z"), substr(vectors[[1]], 1, 1))
+nrow <- length(dimnames[[1]])
+vectors <- lapply(vectors, parse_line, dimnames = dimnames, nrow = nrow)
+
+iterations <- max(abs(unlist(vectors)))
+results <- vapply(vectors, minimize, FUN.VALUE = numeric(1), iterations = iterations)
+part1 <- which.min(results) - 1
+print(part1)
+
+vectors <- lapply(vectors, asplit, MARGIN = 2) |> do.call(what = rbind)
+part2 <- simulate(vectors, iterations = iterations) |>
+    suppressWarnings()
+print(part2)
---
+++ solutions/day24.py
@@ -0,0 +1,90 @@
+with open("inputs/day24.txt") as file:
+    raw = file.read().splitlines()
+
+
+class Component:
+    def __init__(self, inpt):
+        self.lhs, self.rhs = [int(x) for x in inpt.split("/")]
+        self.weight = self.lhs + self.rhs
+        self.ports = {self.lhs, self.rhs}
+
+    def __repr__(self):
+        return f"{self.lhs}-+-{self.rhs}"
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Component)
+            and len(self.ports.intersection(other.ports)) > 0
+        )
+
+    def __ne__(self, other):
+        return self.lhs != other.lhs or self.rhs != other.rhs
+
+    def __hash__(self):
+        return hash((self.lhs, self.rhs))
+
+    def reverse(self):
+        tmp = self.rhs
+        self.rhs = self.lhs
+        self.lhs = tmp
+        return self
+
+
+class Span:
+    def __init__(self, component):
+        if component.rhs == 0:
+            self.end = component.reverse()
+        elif component.lhs == 0:
+            self.end = component
+        else:
+            raise ValueError
+        self.weight = self.end.weight
+
+    def extend(self, new):
+        if new.lhs == self.end.rhs:
+            self.end = new
+        elif new.rhs == self.end.rhs:
+            self.end = new.reverse()
+        else:
+            raise ValueError
+        self.weight = self.weight + self.end.weight
+
+    def __repr__(self):
+        return f"{self.end.__repr__()} (weight {self.weight})"
+
+
+def build_outer(components):
+    found = []
+
+    # Inner recursive function
+    def build(end, components, weight, length):
+        # pdb.set_trace()
+        for i, component in enumerate(components):
+            if end.rhs in component.ports:
+                if end.rhs == component.lhs:
+                    next_c = component
+                else:
+                    next_c = component.reverse()
+                build(
+                    next_c,
+                    list(set(components) - {components[i]}),
+                    weight + next_c.weight,
+                    length + 1,
+                )
+            else:
+                pass
+        found.append({"weight": weight, "length": length})
+
+    init = Component("0/0")
+    build(init, components, weight=init.weight, length=0)
+    maxes = tuple(max(found, key=lambda x: x[attr]) for attr in ("weight", "length"))
+    part2 = max(
+        bridge["weight"] for bridge in found if bridge["length"] == maxes[1]["length"]
+    )
+    return (maxes[0]["weight"], part2)
+
+
+parsed = list(map(lambda x: Component(x), raw))
+result = tuple(build_outer(parsed))
+print(result[0])
+print(result[1])
