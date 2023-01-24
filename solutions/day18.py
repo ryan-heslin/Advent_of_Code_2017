854fb3594b4b4985bb94a21d031053f6905ff866
ryan-heslin <rwheslin@gmail.com>
2023-01-24 11:54:48 -0500

    Progress

--- solutions/day18.py
+++ solutions/day18.py
@@ -107,8 +107,6 @@ def part2(instructions):
         i = 0
         registers = defaultdict(lambda: 0)
         registers["p"] = initial
-        output = deque([])
-        input = deque([])
 
         while 0 <= i < length:
             args = instructions[i]
@@ -117,41 +115,83 @@ def part2(instructions):
             if args[0] == "snd":
                 i += 1
                 sent[initial] += 1
-                output.appendleft(registers.get(args[1], args[1]))
+                yield registers.get(args[1], args[1])
             # Yield values at first "rcv"
             elif args[0] == "rcv":
                 i += 1
-                if len(input) == 0:
-                    yield from output.copy()
-                    input = yield
-                    output = deque([])
-                registers[args[1]] = input.pop()
+                new_val = yield True
+                registers[args[1]] = new_val
 
             else:
                 i = mapping[args[0]](*args[1:], i)
-        yield None
+        yield False
 
     program0 = program(0)
     program1 = program(1)
+    programs = {
+        0: {"program": program0, "queue": deque([]), "exhausted": False},
+        1: {"program": program1, "queue": deque([]), "exhausted": False},
+    }
+
+    # Run both to first receive
+    while not programs[0]["exhausted"]:
+        new_value = next(programs[0]["program"])
+        if type(new_value) == int:
+            programs[1]["queue"].appendleft(new_value)
+        else:
+            if not new_value:
+                programs[0]["exhausted"] = True
+            break
 
     while True:
-        for_1 = next(program0)
-        for_0 = next(program1)
-        # No data available yet
-        if for_0 is None or for_1 is None or not (len(for_0) or len(for_1)):
+        new_value = next(programs[1]["program"])
+        if type(new_value) == int:
+            programs[1]["queue"].appendleft(new_value)
+        else:
+            if not new_value:
+                return sent[1]
             break
-        if len(for_0):
-            print("for_0")
-            print(for_0)
-            print("\n")
-            program0.send(for_0)
-        if len(for_1):
-            print("for_1")
-            print(for_1)
-            print("\n")
-            program1.send(for_1)
 
-    return sent[1]
+    while True:
+        # Run until needs data
+        # False means program completed
+        print(programs)
+        # Deadlock - no data available
+        if not (programs[0]["queue"] or programs[1]["queue"]):
+            return sent[1]
+
+        print(programs)
+        next_value = True
+        while programs[1]["queue"]:
+            # Receiving new value
+            if type(next_value) == int:
+                # Sending new value
+                programs[0]["queue"].appendleft(next_value)
+                next_value = next(programs[1]["program"])
+            else:
+                if next_value:
+                    next_value = programs[1]["program"].send(programs[1]["queue"].pop())
+                else:
+                    return sent[1]
+
+        print(programs[0])
+        if not programs[0]["exhausted"]:
+            next_value = True
+            while programs[0]["queue"]:
+                if type(next_value) == int:
+                    # Sending new value
+                    programs[1]["queue"].appendleft(next_value)
+                    next_value = next(programs[0]["program"])
+                else:
+                    if next_value:
+                        next_value = programs[0]["program"].send(
+                            programs[0]["queue"].pop()
+                        )
+                    else:
+                        programs[0]["exhausted"] = True
+                        break
+        print("---------------")
+        # breakpoint()
 
 
 with open("inputs/day18.txt") as f:
@@ -165,3 +205,4 @@ part2 = part2(instructions)
 print(part2)
 
 # TODO ensure functions can actually write registers
+# 132 too low
